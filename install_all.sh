868c083dd3ecf0a493b6790f3719e727353d0e7c
Kristian Rune Larsen <kristian.rune.larsen@gmail.com>
2011-10-04 12:55:47 +0000

    remove build directory before rebuilding
    git-svn-id: https://python-bufr.googlecode.com/svn/trunk@122 ba0b150a-ca07-11de-8f3f-2f730f80caef

--- install_all.sh
+++ install_all.sh
@@ -5,6 +5,6 @@ echo "Using python as $py"
 list="bufr xml-marshall sqlalchemy-marshall metadb transform"
 for p in $list; do
    echo "======= Now in $p"
-   cd $p && python setup.py build && python setup.py install && cd ..
+   cd $p && rm -r build && python setup.py build && python setup.py install && cd ..
    echo "======= Done with $p"
 done
