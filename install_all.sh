6fd9b839754405bc8ed05de65fe8da7433a2f657
Kristian Rune Larsen <kristian.rune.larsen@gmail.com>
2011-10-06 08:56:47 +0000

    force remove of old build dirs
    git-svn-id: https://python-bufr.googlecode.com/svn/trunk@123 ba0b150a-ca07-11de-8f3f-2f730f80caef

--- install_all.sh
+++ install_all.sh
@@ -5,6 +5,6 @@ echo "Using python as $py"
 list="bufr xml-marshall sqlalchemy-marshall metadb transform"
 for p in $list; do
    echo "======= Now in $p"
-   cd $p && rm -r build && python setup.py build && python setup.py install && cd ..
+   cd $p && rm -rf build && python setup.py build && python setup.py install && cd ..
    echo "======= Done with $p"
 done
